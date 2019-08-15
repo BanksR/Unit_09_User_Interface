3817d8a1e91d08d85c19cab1caa98ed37c409fb3
Rick <r.banks@burnley.ac.uk>
2019-08-15 13:59:58 +0100

    Made a start on the Window Manager Class

---
+++ Unit_09_User_Interface/Assets/Scripts/UI_Scripts/WindowManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowManager : MonoBehaviour 
+{
+
+    public Window[] windows;
+
+    private Window _window;
+    public Window Window
+    {
+        get {return _window; }
+        set {_window = value; }
+    }
+
+
+
+    public static WindowManager instance;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+
+	}
+	
+	
+
+
+
+
+}
